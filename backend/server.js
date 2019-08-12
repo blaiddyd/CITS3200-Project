66a61d6291a75bd47f29be3d78660928b453a764
Pixeladed <vietminhle98@gmail.com>
2019-08-12 13:39:54 +0800

    refactor(server): move route setup outside of startServer

--- backend/server.js
+++ backend/server.js
@@ -6,21 +6,22 @@ const cors = require('cors')
 const port = process.env.PORT || 5000
 const dev = process.env.NODE_ENV !== 'production'
 
+const nextApp = next({ dev })
+const nextHandler = nextApp.getRequestHandler()
+const app = express()
+
+app.use(express.json())
+app.use(helmet())
+app.use(cors({ origin: true }))
+app.get('*', (req, res) => nextHandler(req, res))
+
 exports.startServer = async () => {
-  const nextApp = next({ dev })
-  const nextHandler = nextApp.getRequestHandler()
-
   await nextApp.prepare()
-
-  const app = express()
-  app.use(express.json())
-  app.use(helmet())
-  app.use(cors({ origin: true }))
-
-  app.get('*', (req, res) => nextHandler(req, res))
 
   app.listen(port, error => {
     if (error) throw error
     console.log(`Server listening on port ${port}`)
   })
 }
+
+exports.app = app
