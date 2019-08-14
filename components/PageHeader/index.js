3e6dca3d486a8a12fef1369ac2308c1752b45404
Pixeladed <vietminhle98@gmail.com>
2019-08-14 14:28:54 +0800

    feat(component): add page header component

--- components/PageHeader/index.js
+++ components/PageHeader/index.js
@@ -1,15 +1,17 @@
 import React from 'react'
 
 const PageHeader = props => {
-  const { title = '', subtitle, extra } = props
+  const { title = '', subtitle, extra, ...rest } = props
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-6">
-          {subtitle && <small>{subtitle}</small>}
-          <h3>{title}</h3>
+    <div className="py-3 border-bottom" {...rest}>
+      <div className="container">
+        <div className="row">
+          <div className="col-6">
+            {subtitle && <small>{subtitle}</small>}
+            <h3>{title}</h3>
+          </div>
+          {extra && <div className="col-6">{extra}</div>}
         </div>
-        {extra && <div className="col-6">{extra}</div>}
       </div>
     </div>
   )
