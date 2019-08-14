8c553582aa5b3267adfe07ab08ae8478c13bc0f2
Pixeladed <vietminhle98@gmail.com>
2019-08-14 20:47:16 +0800

    fix(lint): remove tests from eslint ignore

--- mocha-tests/basic.test.js
+++
@@ -1,8 +0,0 @@
-const chai = require("chai");
-const assert = chai.assert;
-
-describe("My basic tests", () => {
-  it("works", () => {
-    assert.isTrue(true);
-  });
-});
---
+++ tests/mocha/basic.test.js
@@ -0,0 +1,8 @@
+const chai = require('chai')
+const assert = chai.assert
+
+describe('My basic tests', () => {
+  it('works', () => {
+    assert.isTrue(true)
+  })
+})
