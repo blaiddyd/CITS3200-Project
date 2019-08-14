c215f2d89c6d86eda34472d1d54a6ca3f3febd2f
Pixeladed <vietminhle98@gmail.com>
2019-08-14 14:38:45 +0800

    feat(pages): add basic modules list page

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Link from 'next/link'
 
+/**
+ * The navbar
+ */
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light border-bottom">
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import PageHeader from '../components/PageHeader'
+import ModuleGrid from '../components/ModuleGrid'
 
 const Index = () => {
   return (
     <div>
       <PageHeader title="Modules" />
+      <div className="py-3">
+        <ModuleGrid />
+      </div>
     </div>
   )
 }
