953f89b071b8e9c4f5fc2998ae27da3e46afebae
systemvaz <systemvaz@outlook.com>
2019-08-17 00:42:15 +0800

    added progressRouter

--- backend/routers/index.js
+++ backend/routers/index.js
@@ -2,9 +2,11 @@
 
 const imageRouter = require('./imageRouter')
 const projectRouter = require('./projectRouter')
+const progressRouter = require('./progressRouter')
 const router = require('express').Router()
 
 router.use('/images', imageRouter)
 router.use('/projects', projectRouter)
+router.use('/progress', progressRouter)
 
 module.exports = router
