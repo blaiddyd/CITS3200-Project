4e835c1a3abd40efe285367f3aaef453cd9c8cf6
Ekin Bukulmez <ekinbukulmez@Ekins-MBP.lan>
2019-09-18 12:10:22 +0800

    (video-upload) refactored testRoute

--- backend/routers/videoRouter.js
+++ backend/routers/videoRouter.js
@@ -1,6 +1,12 @@
+'use strict'
+
 const router = require('express').Router()
 const multer = require('../middleware/multer')
 const GCSUpload = require('../middleware/gcsUpload')
+
+const annotateVideo = require('../../helpers/annotateVideo')
+const videoGetTxt = require('../../helpers/videoGetTxt')
+const videoGetCsv = require('../../helpers/videoGetCsv')
 
 const videoModel = require('../models/videoModel')
 const Video = require('mongoose').model(videoModel.modelName)
@@ -16,6 +22,55 @@ router.post('/', multer.single('video'), GCSUpload, async (req, res) => {
     const newVid = await new Video({ title, filename, url }).save()
     console.log(`Video ${newVid._id} has been saved to the database`)
     res.status(200).json(newVid)
+  } catch (error) {
+    res.status(400).json({ error })
+  }
+})
+
+router.get('/get_text', async (req, res) => {
+  const { id, apiKey } = req.body
+  const video = await Video.findOne({ _id: id })
+
+  if (!video) {
+    console.log(`No video with ID ${video._id} exists`)
+    res.status(400).json({ msg: `No video with ID ${video._id} exists`})
+    return
+  }
+
+  try {
+    const labels = await annotateVideo(video._id, apiKey)
+    const text = videoGetTxt(labels)
+
+    res.set({
+      'Content-Disposition': 'attachment; filename="out.txt"',
+      'Content-Type': 'text/plain'
+    })
+    res.send(text)
+
+  } catch (error) {
+    res.status(400).json({ error })
+  }
+})
+
+router.get('/get_csv', async (req, res) => {
+  const { id, apiKey } = req.body
+  const video = await Video.findOne({ _id: id})
+
+  if (!video) {
+    console.log(`No video with ID ${video._id} exists`)
+    res.status(400).json({ msg: `No video with ID ${video._id} exists`})
+    return
+  }
+
+  try {
+    const labels = await annotateVideo(video._id, apiKey)
+    const csv = videoGetCsv(labels)
+    res.set({
+      'Content-Disposition': 'attachment; filename="out.csv"',
+      'Content-Type': 'text/csv'
+    })
+    res.send(csv)
+
   } catch (error) {
     res.status(400).json({ error })
   }
