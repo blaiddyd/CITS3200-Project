691eed62230c87309df072a07a6a0b7283f2269f
Pixeladed <vietminhle98@gmail.com>
2019-09-23 17:59:23 +0800

    modify project router to use modules

--- backend/routers/projectRouter.js
+++ backend/routers/projectRouter.js
@@ -6,27 +6,10 @@ router.use(require('express').json())
 const multer = require('../middleware/multer')
 const GCSUpload = require('../middleware/gcsUpload')
 const Project = require('mongoose').model('project')
-const Image = require('mongoose').model('image')
-const videoModel = require('../models/videoModel')
-const Video = require('mongoose').model(videoModel.modelName)
-
+const Resource = require('mongoose').model('resource')
 const { modulesMap } = require('../../modules')
 
 router.use(require('express').json())
-
-/* 
-    This route gets all projects from MongoDB
-    inp => A GET request to the route
-    out => All saved projects on MongoDB
-*/
-router.get('/', async (req, res) => {
-  try {
-    const projects = await Project.find()
-    res.status(200).json(projects)
-  } catch (error) {
-    res.status(400).json({ error })
-  }
-})
 
 /* 
     This route adds a single project to MongoDB
@@ -53,7 +36,7 @@ router.post('/', async (req, res) => {
     inp => A request, with an image item connected to the body key of "image"
     out => The saved Image model on MongoDB
 */
-router.post('/:id', multer.single('image'), GCSUpload, async (req, res) => {
+router.post('/:id', multer.single('resource'), GCSUpload, async (req, res) => {
   const { originalname: title, gcsName: filename, gcsUrl: url } = req.file
   try {
     const currentProj = await Project.findOne({ _id: req.params.id })
@@ -61,106 +44,11 @@ router.post('/:id', multer.single('image'), GCSUpload, async (req, res) => {
       res.status(400).json({ msg: 'Invalid project ID.' })
       return
     }
-    const newImage = await new Image({ title, filename, url }).save()
-    currentProj.imageIDs.push(newImage._id)
+    const newResource = await new Resource({ title, filename, url }).save()
+    currentProj.resourceIDs.push(newResource._id)
     await currentProj.save()
-    console.log(`Image ${newImage._id} saved to project ${req.params.id}`)
-    res.status(200).json(newImage)
-  } catch (error) {
-    res.status(400).json({ error })
-  }
-})
-
-/**
- * This router uploads a single video to GCS and adds it with the project key
- */
-router.post(
-  '/:id/video',
-  multer.single('video'),
-  GCSUpload,
-  async (req, res) => {
-    const { originalname: title, gcsName: filename, gcsUrl: url } = req.file
-    try {
-      const project = await Project.findOne({ _id: req.params.id })
-      if (!project) {
-        res.status(400).json({ msg: 'Invalid project ID. ' })
-        return
-      }
-
-      const newVid = await new Video({ title, filename, url }).save()
-      project.videoID = newVid._id
-      const newP = await project.save()
-      console.log(url, newVid._id, newP.videoID)
-      console.log(`Video ${newVid._id} saved to project ${req.params.id}`)
-      res.status(200).json(newVid)
-    } catch (err) {
-      res.status(400).json({ err })
-    }
-  }
-)
-
-/* 
-    This route gets a single project from MongoDB
-    inp => A GET request, with the id as parameter
-    out => The saved project on MongoDB with the id
-*/
-router.get('/:id', async (req, res) => {
-  try {
-    const { id } = req.params
-    const project = await Project.findOne({ _id: id })
-    res.status(200).json({ project })
-  } catch (error) {
-    res.status(400).json({ error })
-  }
-})
-
-/* 
-    This route updates an image model on MongoDB
-    inp => A request, with body {title, ImageIDs} and parameters { id }
-    out => The saved Image model on MongoDB
-*/
-router.patch('/:id', async (req, res) => {
-  try {
-    const { id } = req.params
-    const { title, imageIDs, apiKey } = req.body
-
-    console.log(imageIDs)
-
-    const project = await Project.findOne({ _id: id })
-    if (!project) {
-      return res
-        .status(400)
-        .json({ msg: 'No project exists with the given id.' })
-    }
-
-    if (title) project.title = title
-    if (imageIDs) project.imageIDs = imageIDs
-    if (apiKey) project.apiKey = apiKey
-
-    const newProject = project.save()
-    console.log(`Project ${id} updated.`)
-    res.status(200).json(newProject)
-  } catch (error) {
-    res.status(400).json({ error })
-  }
-})
-/* 
-    This route deletes a project from MongoDB
-    inp => A DELETE request to this route with id as param
-    out => A success or error message
-*/
-router.delete('/:id', async (req, res) => {
-  try {
-    const { id } = req.params
-    const project = Project.findOne({ _id: id })
-    if (!project) {
-      return res
-        .status(400)
-        .json({ msg: 'No project exists with the given id.' })
-    }
-    await project.remove()
-    console.log(`Project ${id} deleted.`)
-    res.status(200).json({ mgs: `Project ${id} deleted.` })
+    console.log(`Resource ${newResource._id} saved to project ${req.params.id}`)
+    res.status(200).json(newResource)
   } catch (error) {
     res.status(400).json({ error })
   }
@@ -171,9 +59,10 @@ router.delete('/:id', async (req, res) => {
     inp => A GET request to this route with param = ID
     out => The updated image model
 */
-router.get('/annotate/:id', async (req, res) => {
+router.get('/annotate/:id/:slug', async (req, res) => {
   try {
-    const project = await Project.findOne({ _id: req.params.id })
+    const { id, slug } = req.params
+    const project = await Project.findOne({ _id: id })
     if (!project) {
       return res
         .status(400)
@@ -181,10 +70,10 @@ router.get('/annotate/:id', async (req, res) => {
     }
 
     // NOTE: not await to run in background
-    const processor = modulesMap.get('Ecological Image Classification')
+    const processor = modulesMap.get(slug)
     processor.process(project)
 
-    res.json({ msg: `Annotating project ${req.params.id} in the background.` })
+    res.json({ msg: `Annotating project ${id} in the background.` })
   } catch (error) {
     console.error(error)
     res.status(400).json({ error })
@@ -196,19 +85,18 @@ router.get('/annotate/:id', async (req, res) => {
     inp => A GET request to this route with param = match type
     out => A zip file of matches
 */
-router.get('/download/:id/:match', async (req, res) => {
+router.get('/download/:id/:slug/:type', async (req, res) => {
   try {
-    const project = await Project.findOne({ _id: req.params.id })
+    const { id, slug, type } = req.params
+    const project = await Project.findOne({ _id: id })
     if (!project) {
       res.status(400).json({ msg: 'Project ID not found.' })
     }
 
-    const type = req.params.match
-
-    const processor = modulesMap.get('Ecological Image Classification')
+    const processor = modulesMap.get(slug)
     const filename = await processor.download(project, type)
 
-    res.download(filename, `${type}.zip`)
+    res.download(filename, `${type}`)
   } catch (error) {
     res.status(400).json({ error })
   }
--- modules/index.js
+++ modules/index.js
@@ -4,7 +4,7 @@ const VideoModule = require('./video')
 const modules = [ImageModule, VideoModule]
 
 const modulesMap = new Map()
-modules.forEach(m => modulesMap.set(m.name, m))
+modules.forEach(m => modulesMap.set(m.slug, m))
 
 module.exports = modules
 module.exports.modulesMap = modulesMap
